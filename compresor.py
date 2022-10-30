68ed037f99bbae280d5b4fe6500b573f1171284d
Diego Gomez <diegocor4@gmail.com>
2022-10-30 17:44:36 -0300

    script finalizado

--- compresor.py
+++ compresor.py
@@ -1,7 +1,15 @@
+from distutils import extension
+from pickletools import optimize
 from PIL import Image # python3 -m pip install Pillow
 
 import os
 
 downloadsFolder = "C:\Users\diego\Downloads"
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+    for filename in os.listdir(downloadsFolder):
+        name, extension = os.path.splitext(downloadsFolder + filename)
+
+        if extension in [".jpg",".jpeg",".png"]:
+            picture = Image.open(downloadsFolder + filename)
+            picture.save(downloadsFolder + "comprimido" + filename, optimize = True, quality = 60)
