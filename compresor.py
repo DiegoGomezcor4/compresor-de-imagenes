cc0d8d4b1fb00722b44d7af2e8f326c3d094a3e4
Diego Gomez <diegocor4@gmail.com>
2022-10-31 00:22:59 -0300

    compresor y reubicador de archivos terminado

--- compresor.py
+++ compresor.py
@@ -5,6 +5,7 @@ from PIL import Image # python3 -m pip install Pillow
 import os
 
 downloadsFolder = "/Users/Diego/Downloads/"
+picturesFolder = "/Users/diego/OneDrive/Imágenes/"
 
 if __name__ == "__main__":
     for filename in os.listdir(downloadsFolder):
@@ -12,4 +13,10 @@ if __name__ == "__main__":
 
         if extension in [".jpg",".jpeg",".png"]:
             picture = Image.open(downloadsFolder + filename)
-            picture.save(downloadsFolder + "comprimido" + filename, optimize = True, quality = 60)
+            picture.save(picturesFolder + "comprimido_" + filename, optimize = True, quality = 60)
+            os.remove(downloadsFolder + filename)
+            print(name + ": " + extension)
+
+        if extension in [".mp3"]:
+            musicFolder = "/Users/Diego/Music/"
+            os.rename(downloadsFolder + filename, musicFolder + filename)
